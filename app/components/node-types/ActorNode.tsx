cd210db0b9bbb68f6b77025c569a167334e43c91
Matteo Veraldi <mattveraldi@gmail.com>
2023-07-05 00:39:35 +0200

    feat: add input text

--- app/components/node-types/ActorNode.tsx
+++ app/components/node-types/ActorNode.tsx
@@ -3,12 +3,11 @@ import { CreateUserSVG } from "../svgs";
 
 export function UserNode() {
   return (
-    <div>
-      <Handle className="opacity-30" type="source" position={Position.Top} />
-      <Handle className="opacity-30" type="source" position={Position.Right} />
-      <Handle className="opacity-30" type="target" position={Position.Bottom} />
-      <Handle className="opacity-30" type="target" position={Position.Left} />
-      <div className="p-2">{CreateUserSVG}</div>
+    <div className="flex flex-col items-center">
+      <Handle className="opacity-30" type="target" position={Position.Right} />
+      <Handle className="opacity-30" type="source" position={Position.Left} />
+      <div className="p-2 pb-1">{CreateUserSVG}</div>
+      <input className="text-xs w-6" defaultValue="user" />
     </div>
   );
 }
